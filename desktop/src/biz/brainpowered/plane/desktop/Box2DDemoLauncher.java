9a1c94a0d98706ac1d58fb7413ba82a1b54ec5c6
Sebastian Scholle <sebastian@rightshift.biz>
2015-01-28 13:35:54 +0200

    Updates and Box2dDemo Added

--- desktop/src/biz/brainpowered/plane/desktop/Box2DDemoLauncher.java
+++ desktop/src/biz/brainpowered/plane/desktop/Box2DDemoLauncher.java
@@ -1,17 +1,17 @@
 package biz.brainpowered.plane.desktop;
 
-import biz.brainpowered.plane.BloomDemo;
+import biz.brainpowered.plane.Box2dDemo;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class BloomDemoLauncher{
+public class Box2dDemoLauncher{
     public static void main (String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "PlanePilot";
         //config.useGL20 = true;
-        config.width = 700;
-        config.height = 1024;
-        new LwjglApplication(new BloomDemo(), config);
+        config.width = (int) (Box2dDemo.V_WIDTH * Box2dDemo.SCALE);
+        config.height = (int) (Box2dDemo.V_HEIGHT * Box2dDemo.SCALE);
+        new LwjglApplication(new Box2dDemo(), config);
     }
 
 }
